e14e2ea8dc40057f830e1606f28e598e59869a06
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-07-06 00:57:19 +0200

    do tasks

---
+++ src/basic/3.ts
@@ -0,0 +1,7 @@
+type UnionType = string | number;
+let stringOrNumber: UnionType = 5;
+stringOrNumber = "";
+
+type AbleType = "enable" | "disable";
+let isAble: AbleType = "enable";
+isAble = "disable";
